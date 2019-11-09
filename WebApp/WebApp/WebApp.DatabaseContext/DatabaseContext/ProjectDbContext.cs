68a9eb94f2e3c7053105c162af1291ac081caebc
HOSSAIN-ARMAN <hossainarman4389@gmail.com>
2019-11-09 13:56:11 +0600

    Prectice entity query

---
+++ WebApp/WebApp/WebApp.DatabaseContext/DatabaseContext/ProjectDbContext.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebApp.Model.Model;
+
+namespace WebApp.DatabaseContext.DatabaseContext
+{
+    public class ProjectDbContext: DbContext
+    {
+        public ProjectDbContext()
+        {
+            Configuration.LazyLoadingEnabled = false; // Disable Lazy Loading
+        }
+
+        public DbSet<Student> Students { set; get; }
+        public DbSet<Department> Departments { set; get; }
+
+    }
+}
